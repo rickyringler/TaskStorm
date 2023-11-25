36f382f9faacc5d2e906abfc300fd655a322831c
Ricky Ringler <135162902+rickyringler@users.noreply.github.com>
2023-11-25 11:20:37 -0500

    v1

--- TaskStorm/main.cpp
+++ TaskStorm/main.cpp
@@ -8,11 +8,12 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-
 #include <windows.h>
 
 #pragma once
 #pragma warning( disable : 4996 )
+
+bool main_menu = false;
 
 int taskCount = 0;
 std::vector<Task> taskArray;
@@ -26,37 +27,28 @@ time_t now = time(0);
 char* dt = ctime(&now);
 std::ofstream taskDatabase;
 
-
-int main(int argc, char** argv) {
-
-    //Start user process
-    for (char& c : titleScreen) {
-        Sleep(120);
-        std::cout << (c);
-    }
-
-    Sleep(200);
-
+void login()
+{
     User user;
-
     std::cout << "\nWhat is your username?\n" << std::endl;
     std::getline(std::cin, currentUser);
     user.setName(currentUser);
     user.processUser();
     std::cout << "\nWelcome back, " << user.getName() << std::endl;
-    //End user process
-
-    //Start task process
     while (taskCount == 0)
     {
         std::cout << "How many tasks do you want to create for today?" << std::endl;
         std::cin >> taskCount;
         user.setNumberOfTasks(taskCount);
     }
+}
 
+void userSetTasks()
+{
     for (int i = 0; i < taskCount; ++i) {
         Task* task = new Task;
         std::string* namePtr = new std::string;
+        std::cin.ignore();
         std::cout << "\nEnter the task name for Task\n" << i + 1 << ": " << std::endl;
         task->setTaskName();
         *namePtr = "Task" + std::to_string(i + 1);
@@ -73,27 +65,46 @@ int main(int argc, char** argv) {
         // Add the Task object to the array
         taskArray.push_back(*task);
 
-        taskDatabase.open("taskDatabase.csv");
-        taskDatabase << task;
+
+        taskDatabase << task->getTaskName();
         taskDatabase << "," << dt;
     }
+}
 
-
-    // Access the Task objects in the array
-                         
+void mainMenuScreen()
+{
     std::cout << "Main Menu" << std::endl;
     std::cout << "Press 1 to display tasks." << std::endl;
     std::cout << "Press 2 to end program." << std::endl;
 
-    std::getline(std::cin, userOption);
-
-    if(userOption == "1")
+    std::cin >> userOption;
+    if (userOption == "1")
     {
         for (int i = 0; i < taskCount; ++i) {
             std::cout << "\nTask Information for Task " << i + 1 << ":\n";
             taskArray[i].showTaskDetails();
+            std::cout << i << std::endl;
+            mainMenuScreen();
         }
     }
+    else
+    {
+        mainMenuScreen();
+    }
+}
 
+int main(int argc, char** argv) 
+{
+    taskDatabase.open("taskDatabase.csv", std::ios_base::app);
+    for (char& c : titleScreen) 
+    {
+        Sleep(120);
+        std::cout << (c);
+    }
+
+    login();
+    userSetTasks();
+    mainMenuScreen();
+                         
     return 0;
 }
